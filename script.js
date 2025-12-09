44297663f1cae8d1f53308ab382827831b9e2ca4
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-12-09 09:30:33 +0530

    updated version

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let done1 = false;
+let done2 = false;
+
 function task1(callback) {
     console.log("Task - 1 Scheduled")
     setTimeout(() => {
+        done1 = true;
         callback();
+        bothDone()
     }, 2000);
     // 2 second timeout then callback("Task 1 done")
 }
@@ -10,7 +15,9 @@ function task2(callback) {
     // 1 second timeout then callback("Task 2 done")
     console.log("Task - 2 Scheduled")
     setTimeout(() => {
+        done2 = true;
         callback();
+        bothDone()
     }, 3000);
 }
 
@@ -20,10 +27,11 @@ task1(function () {
     console.log("Task 1 Completed ✅")
     task2(function () {
         console.log("Task 2 Completed ✅")
-        bothDone()
     })
 })
 
 function bothDone() {
-    console.log("Both done")
+    if (done1 && done2) {
+        console.log("Both Done")
+    }
 }
