9b3ae932ddf340f4d7ff8d06642da2ab1ccbe704
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-12-10 09:55:24 +0530

    Number matcher by user and programmer

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-let cards = document.querySelector(".cards");
-const colorArr = ["maroon", "#628141", "#1B211A", "#8BAE66", "#F79A19", "red"];
+let computer = Math.floor(Math.random() * 10) + 1;
+let user;
+do {
+    user = prompt("Please enter the number till 10 to check that is equal to computers nummber or not :")
 
-async function fetchUser() {
-    try {
-        let rawData = await fetch("https://jsonplaceholder.typicode.com/posts");
-        let actualData = await rawData.json();
-        console.log(actualData);
-        let sum = "";
-        actualData.forEach((el) => {
-            let rnum = Math.floor(Math.random() * 5) + 1;
-            sum += `<div style="color:white;background:conic-gradient(${colorArr[rnum]}, ${colorArr[colorArr.length % rnum]},${colorArr[rnum]})" class="card">
-            <h1>ID: <span id="idNum"> ${el.id}</span></h1>
-            <h3>Title : <span id="title"> ${el.title}</span></h3>
-        </div>
-        `;
-        });
-
-        cards.innerHTML = sum;
-    } catch (error) {
-        console.error(error);
+    if (!isNaN(user) && user != null) {
+        if (user > computer) {
+            console.log("Your number is Bigger")
+        } else if (user < computer) {
+            console.log("Your number is smaller")
+        } else if (user == computer) {
+            console.log("Congradulations ! The number is equal")
+            break;
+        }
     }
-}
-
-fetchUser();
+    computer = Math.floor(Math.random() * 10) + 1;
+} while (user != null && user != computer);
