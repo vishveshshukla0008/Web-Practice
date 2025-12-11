b4fb03a53c720cded4ed1b9d82c8d36493c43e7c
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-12-11 18:12:52 +0530

    Windows-home-page-features-repo

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-let computer = Math.floor(Math.random() * 10) + 1;
-let user;
-do {
-    user = prompt("Please enter the number till 10 to check that is equal to computers nummber or not :")
-
-    if (!isNaN(user) && user != null) {
-        if (user > computer) {
-            console.log("Your number is Bigger")
-        } else if (user < computer) {
-            console.log("Your number is smaller")
-        } else if (user == computer) {
-            console.log("Congradulations ! The number is equal")
-            break;
-        }
+let windowIcon = document.querySelector("#window-icon");
+let windowsPanel = document.querySelector(".windows-panel");
+let main = document.querySelector("main");
+let rightClickCard = document.querySelector(".right-click-card");
+let windowModalOpen = false;
+windowIcon.addEventListener("click", () => {
+    if (windowModalOpen) {
+        windowModalOpen = false;
+        windowsPanel.style.transform = "translateY(120%)";
+        console.log("closing")
+    } else {
+        console.log("opening")
+        windowModalOpen = true;
+        windowsPanel.style.transform = "translateY(0%)"
     }
-    computer = Math.floor(Math.random() * 10) + 1;
-} while (user != null && user != computer);
+});
+
+window.oncontextmenu = e => {
+    e.preventDefault()
+    rightClickCard.classList.remove("hide");
+    rightClickCard.style.left = e.clientX + "px"
+    rightClickCard.style.top = e.clientY + "px"
+}
+
+main.addEventListener("click", () => {
+    rightClickCard.classList.add("hide");
+})
+
+
