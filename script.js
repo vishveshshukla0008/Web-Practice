3f6cbdda5ed32b4f6e6df4962c2fa4cc6f6dfb8f
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-12-14 10:33:07 +0530

    DEBOUNCE AND THROTTLE

--- script.js
+++ script.js
@@ -1,57 +1,26 @@
-class YoutubeChannel {
-    constructor() {
-        this.subscribers = [];
-    }
+document.addEventListener("mousemove", throttle(function (e) {
+    console.log(`${e.clientX}, ${e.clientY}`);
+}, 1000))
 
-    subscribe(user) {
-        this.subscribers.push(user);
-        user.update(`${user.name} subscribbed the channel`)
-    }
 
-    unSubscribe(user) {
-        if (!user) {
-            console.error("Provide a user at least");
-            return;
+function throttle(fn, delay) {
+    let timer = 0;
+    return function (e) {
+        let now = Date.now();
+        if (now - timer >= delay) {
+            timer = now;
+            fn(e);
         }
-        let isExists = this.subscribers.some((el) => el.name === user.name);
-        if (!isExists) {
-            console.warn("Please subscribe First !!")
-            return;
-        }
-        this.subscribers = this.subscribers.filter((el) => el.name !== user.name);
-        console.warn(`${user.name} is unsubscribbed your channel`)
-    }
-
-    showAllSubscribers() {
-        console.log(this.subscribers)
-    }
-
-    addVideo() {
-        this.notify("New Video is out now !!!")
-    }
-    notify(data) {
-        this.subscribers.forEach((user) => user.update(data))
     }
 }
 
-class User {
-    constructor(name) {
-        this.name = name;
-    }
-    update(data) {
-        console.log(`${this.name} ` + data)
+function debounce(fn, delay) {
+    let timer;
+
+    return function (e) {
+        clearInterval(timer)
+        timer = setTimeout(() => {
+            fn(e)
+        }, 2000)
     }
 }
-
-let yashcodes = new YoutubeChannel();
-let user1 = new User("Ram");
-let user2 = new User("Shyam");
-let user3 = new User("GhanShyam");
-
-yashcodes.subscribe(user1);
-yashcodes.subscribe(user2);
-yashcodes.subscribe(user3);
-yashcodes.addVideo()
-
-
-// yashcodes.showAllSubscribers()
